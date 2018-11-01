967cc2fa1c8d2d895fd2b280809839620f41cd97
MGR Surface <marcin.grom@gmail.com>
2018-11-01 22:32:58 +0100

    few fixes

--- giossync/__init__.py
+++ giossync/__init__.py
@@ -6,18 +6,17 @@ import async_timeout
 BASE_URL = 'http://api.gios.gov.pl/pjp-api/rest/'
 SENSORS_URL = BASE_URL + 'station/sensors/{}/'
 SENSOR_DATA_URL = BASE_URL + 'data/getData/{}/'
+LOCATIONS_URL = BASE_URL + 'station/findAll'
 
 
 class GiosClient(object):
     """Gios client implementation."""
 
-    def __init__(self, station, session=None, timeout=aiohttp.client.DEFAULT_TIMEOUT):
+    def __init__(self, session=None, timeout=aiohttp.client.DEFAULT_TIMEOUT):
         """Constructor.
         
-        station: Station Id from http://powietrze.gios.gov.pl/pjp/current#
         session: aiohttp.ClientSession or None to create a new session.
         """
-        self._station = station
         self._timeout = timeout
         if session is not None:
             self._session = session
@@ -35,8 +34,20 @@ class GiosClient(object):
         return (yield from self._get(SENSOR_DATA_URL.format(sensor_id)))
 
     @asyncio.coroutine
+    def get_locations(self):
+        """Get available locations."""
+        return (yield from self._get(LOCATIONS_URL))
+
+    @asyncio.coroutine
     def _get(self, path, **kwargs):
         with async_timeout.timeout(self._timeout):
-            resp = yield from self._session.get(
-                path, params=dict(self._params, **kwargs))
-            return (yield from resp.json())['data']
+            resp = yield from self._session.get(path)
+            return (yield from resp.json())
+    
+    async def get_location_data(self, loc):
+        locations = await self.get_locations()
+        for location in locations:
+            if location['id'] == loc:
+                return location
+        else:
+            return None
